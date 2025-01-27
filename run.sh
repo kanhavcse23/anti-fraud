066b98185b9cc9bb365e24bb75bacb39723c79f9
kanhavcse23 <kanhav.cse23@gmail.com>
2025-01-27 12:36:28 +0530

    fix docker

--- run.sh
+++ run.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-gh repo clone kanhavcse23/anti-fraud
-# Activate the virtual environment
-source rishav_project_env/bin/activate
+python3 -m venv rishavenv
+source rishavenv/bin/activate
 
-# Your commands here
-# Example:
 sudo apt update
+sudo apt install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo apt install -y golang-go
 go mod tidy
 sudo systemctl start docker
+sudo apt  install docker-compose
 docker-compose up --build
-
-# Deactivate the virtual environment
-# deactivate
+deactivate
